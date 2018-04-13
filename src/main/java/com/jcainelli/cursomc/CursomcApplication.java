b741aedd8ae00105df17f3fb848e3412982bd8f9
Jean Cainelli <jcainelli@gmail.com>
2018-04-12 22:39:22 -0300

    Criando operação de intanciação

--- src/main/java/com/jcainelli/cursomc/CursomcApplication.java
+++ src/main/java/com/jcainelli/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.jcainelli.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.jcainelli.cursomc.domain.Categoria;
+import com.jcainelli.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat3 = new Categoria(null, "Celulares"); 			
+		categoriaRepository.saveAll(Arrays.asList(cat3));
+	}
+	
+	
 }
